47ca3d56292c08e3693f15cc3af480af96d6f341
Max Lübbering <le1nux@users.noreply.github.com>
2020-11-28 16:29:14 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setup(
     name='mlgym',
-    version='0.0.1',
+    version='0.0.2',
     author='Max Luebbering',
     description="MLgym, a python framework for distributed machine learning model training in research.",
     long_description=long_description,
