56cc86943785a638fe0f6e154248c21f53b8c039
Max Luebbering <m.luebbering@gmail.com>
2020-11-28 14:34:38 +0100

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='ml_gym',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pytest",
+        "pytest-cov",
+        "torch",
+        "h5py",
+        "tqdm",
+        "pyyaml",
+        "datastack",
+        "scipy", 
+        "dashifyML",
+        "scikit-learn"
+    ],
+    python_requires=">=3.7"
+)
