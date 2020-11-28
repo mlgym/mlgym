a1c141cac858525feeeb4fc3d5284fd75a7a5df1
Max Lübbering <le1nux@users.noreply.github.com>
2020-11-28 16:00:35 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='mlgym',
     version='0.0.1',
+    author='Max Luebbering',
+    description="MLgym, a python framework for distributed machine learning model training in research.",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         "pytest",
@@ -17,5 +22,6 @@ setup(
         "dashifyML",
         "scikit-learn"
     ],
-    python_requires=">=3.7"
+    python_requires=">=3.7",
+    url="https://github.com/le1nux/mlgym"
 )
