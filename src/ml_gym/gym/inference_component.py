fea1fee9d6b764a9ef7a059ef2976b3911c34ab5
Max Luebbering <m.luebbering@gmail.com>
2021-02-17 00:39:46 +0100

    Implemented ExportedModel

--- src/ml_gym/gym/inference_component.py
+++ src/ml_gym/gym/inference_component.py
@@ -4,6 +4,30 @@ from typing import List
 import torch
 from ml_gym.gym.predict_postprocessing_component import PredictPostprocessingComponent
 from ml_gym.gym.post_processing import PredictPostProcessingIF
+import tqdm
+from ml_gym.data_handling.dataset_loader import DatasetLoader
+
+
+class ExportedModel:
+    def __init__(self, model: NNModel, post_processors: List[PredictPostProcessingIF]):
+        self.post_processors = post_processors
+        self.model = model
+
+    def predict_tensor(self, sample_tensor: torch.Tensor):
+        with torch.no_grad():
+            forward_result = self.model.forward(sample_tensor)
+        result_batch = InferenceResultBatch(targets=None, tags=None, predictions=forward_result)
+        return PredictPostprocessingComponent.post_process(result_batch, post_processors=self.post_processors)
+
+    def predict_dataset_batch(self, batch: DatasetBatch) -> InferenceResultBatch:
+        with torch.no_grad():
+            forward_result = self.model.forward(batch.samples)
+        result_batch = InferenceResultBatch(targets=batch.targets, tags=batch.tags, predictions=forward_result)
+        return PredictPostprocessingComponent.post_process(result_batch, post_processors=self.post_processors)
+
+    def predict_data_loader(self, dataset_loader: DatasetLoader) -> InferenceResultBatch:
+        result_batches = [self.predict_dataset_batch(batch) for batch in tqdm.tqdm(dataset_loader, desc="Batches processed:")]
+        return InferenceResultBatch.combine(result_batches)
 
 
 class InferenceComponent:
@@ -19,3 +43,11 @@ class InferenceComponent:
             forward_result = model.forward(batch.samples)
         result_batch = InferenceResultBatch(targets=batch.targets, tags=batch.tags, predictions=forward_result)
         return PredictPostprocessingComponent.post_process(result_batch, post_processors=self.post_processors)
+
+    def predict_data_loader(self, model: NNModel, dataset_loader: DatasetLoader) -> InferenceResultBatch:
+        result_batches = [self.predict(model, batch) for batch in tqdm.tqdm(dataset_loader, desc="Batches processed:")]
+        return InferenceResultBatch.combine(result_batches)
+
+    # TODO, this is really just a temporary hack and should be done at a different place!
+    def export_model(self, model: NNModel) -> ExportedModel:
+        return ExportedModel(model, self.post_processors)
