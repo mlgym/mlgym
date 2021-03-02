3fd7009505bdb560da8b1fe01bf223a217877141
Max Luebbering <m.luebbering@gmail.com>
2021-03-02 16:04:04 +0100

    create_blueprints allows to add initial_experiment_id as counter initialization

--- src/ml_gym/__init__.py
+++ src/ml_gym/__init__.py
@@ -10,10 +10,10 @@ def create_blueprints(blue_print_class: Type[BluePrint],
                       dashify_logging_path: str,
                       num_epochs: int,
                       grid_search_id: str,
-                      external_injection: Dict[str, Any]) -> List[BluePrint]:
+                      external_injection: Dict[str, Any] = None,
+                      initial_experiment_id: int = 0) -> List[BluePrint]:
 
-    run_mode = AbstractGymJob.Mode[run_mode]
-    run_id_to_config_dict = {str(run_id): config for run_id, config in enumerate(
+    run_id_to_config_dict = {run_id: config for run_id, config in enumerate(
         GridSearch.create_gs_from_config_dict(gs_config))}
     config_tuples = list(run_id_to_config_dict.items())
     epochs = list(range(num_epochs))
@@ -21,7 +21,7 @@ def create_blueprints(blue_print_class: Type[BluePrint],
     blueprints = []
     for run_id, config in config_tuples:
         blue_print = blue_print_class(grid_search_id=grid_search_id,
-                                      run_id=run_id,
+                                      run_id=str(run_id + initial_experiment_id),
                                       epochs=epochs,
                                       run_mode=run_mode,
                                       config=config,
