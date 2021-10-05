fe6642390d34dee9ff9996da286516e59c7f1017
Max Luebbering <m.luebbering@gmail.com>
2021-10-05 18:07:23 +0200

    dataloader now always shuffles

--- src/ml_gym/data_handling/dataset_loader.py
+++ src/ml_gym/data_handling/dataset_loader.py
@@ -11,14 +11,16 @@ from ml_gym.data_handling.postprocessors.collator import Collator
 class DatasetLoaderFactory:
     @staticmethod
     def get_splitted_data_loaders(dataset_splits: Dict[str, InformedDatasetIteratorIF], batch_size: int, collate_fn: Callable = None,
-                                  weigthed_sampling_split_name: str = None, label_pos: int = 2, seeds: List[int] = None) -> Dict[str, "DatasetLoader"]:
+                                  weigthed_sampling_split_name: str = None, label_pos: int = 2, seeds: Dict[str, int] = None) -> Dict[str, "DatasetLoader"]:
+        seeds = {} if seeds is None else seeds
         # NOTE: Weighting is only applied to the split specified by `weigthed_sampling_split_name`.
         data_loaders = {}
         for split_id, (split_name, dataset_split) in enumerate(dataset_splits.items()):
+            seed = seeds[split_name] if split_name in seeds else None
             if split_name == weigthed_sampling_split_name:
-                sampler = SamplerFactory.get_weighted_sampler(dataset_split, label_pos, seeds[split_id])
+                sampler = SamplerFactory.get_weighted_sampler(dataset_split, label_pos, seed)
             else:
-                sampler = SamplerFactory.get_random_sampler(dataset_split, seeds[split_id])
+                sampler = SamplerFactory.get_random_sampler(dataset_split, seed)
 
             data_loaders[split_name] = DatasetLoader(dataset_iterator=dataset_split,
                                                      batch_size=batch_size,
@@ -40,7 +42,7 @@ class SamplerFactory:
         Returns:
             [WeightedRandomSampler]: Instance of WeightedRandomSampler.
         """
-        rnd_generator = torch.Generator().manual_seed(seed)
+        rnd_generator = torch.Generator().manual_seed(seed) if seed is not None  else None
         # get the class weights
         target_counts = Counter([int(sample[label_pos]) for sample in dataset])  # uses generator expression
         target_tuples = [(k, v) for k, v in sorted(target_counts.items(), key=lambda item: item[0])]
@@ -51,7 +53,7 @@ class SamplerFactory:
 
     @staticmethod
     def get_random_sampler(dataset: InformedDatasetIteratorIF, seed: int = 0) -> Sampler:
-        rnd_generator = torch.Generator().manual_seed(seed)
+        rnd_generator = torch.Generator().manual_seed(seed) if seed is not None  else None
         return RandomSampler(data_source=dataset, generator=rnd_generator)
 
 
