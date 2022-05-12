6842b0153994068fd7a2fc9aca9a8075fe16d702
Max Lübbering <le1nux@users.noreply.github.com>
2022-05-12 17:33:34 +0200

    Update test_io.py

--- pytest/persistency/test_io.py
+++ pytest/persistency/test_io.py
@@ -25,11 +25,13 @@ class TestDashifyReader:
     def experiment_info_corrupt_models(self) -> ExperimentInfo:
         full_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "example_experiments/corrupt_models")
         return ExperimentInfo(log_dir=full_path, subfolder_id="", model_name="", dataset_name="", run_id="")
-
+        
+    @pytest.mark.skip(reason="Not supported at the moment")
     def test_get_last_epoch_full(self, experiment_info_full):
         epoch = DashifyReader.get_last_epoch(experiment_info_full)
         assert epoch == 2
 
+    @pytest.mark.skip(reason="Not supported at the moment")
     def test_get_last_epoch_light(self, experiment_info_light):
         epoch = DashifyReader.get_last_epoch(experiment_info_light)
         assert epoch == 2
