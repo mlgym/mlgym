8dd5d7213e2215f21f1a713592053327046fab47
Max Luebbering <le1nux@users.noreply.github.com>
2022-06-07 15:26:42 +0200

    minor refactoring of GridSearchValidator

--- src/ml_gym/validation/gs_validator.py
+++ src/ml_gym/validation/gs_validator.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Type
+from typing import Dict, Any, List, Type
 from ml_gym.blueprints.blue_prints import create_blueprint
 from ml_gym.gym.gym import Gym
 from ml_gym.gym.jobs import AbstractGymJob
@@ -13,8 +13,8 @@ class GridSearchValidator(ValidatorIF):
         self.re_eval = re_eval
         self.keep_interim_results = keep_interim_results
 
-    def run(self, blue_print_type: Type[BluePrint], gym: Gym, gs_config: Dict[str, Any],
-            num_epochs: int, dashify_logging_path: str):
+    def create_blue_prints(self, blue_print_type: Type[BluePrint], job_type: AbstractGymJob.Type, gs_config: Dict[str, Any], num_epochs: int,
+                           dashify_logging_path: str) -> List[Type[BluePrint]]:
         run_id_to_config_dict = {run_id: config for run_id, config in enumerate(GridSearch.create_gs_from_config_dict(gs_config))}
         job_type = AbstractGymJob.Type.STANDARD if self.keep_interim_results else AbstractGymJob.Type.LITE
 
@@ -29,5 +29,11 @@ class GridSearchValidator(ValidatorIF):
                                          experiment_id=config_id,
                                          job_type=job_type)
             blueprints.append(blueprint)
+        return blueprints
+
+    def run(self, blue_print_type: Type[BluePrint], gym: Gym, gs_config: Dict[str, Any],
+            num_epochs: int, dashify_logging_path: str):
+        blueprints = self.create_blue_prints(blue_print_type=blue_print_type, gym=gym, gs_config=gs_config,
+                                             num_epochs=num_epochs, dashify_logging_path=dashify_logging_path)
         gym.add_blue_prints(blueprints)
         gym.run(parallel=True)
