f33db015758fbf67112733bde1a19ec881c16a8f
zengxyu <sduzengxiangyu@outlook.com>
2022-06-21 23:28:18 +0200

    TestMetricFactory

---
+++ pytest/loss_functions/test_multi_loss.py
@@ -0,0 +1,4 @@
+from ml_gym.loss_functions.loss_functions import LossWarmupMixin
+from ml_gym.loss_functions.multi_term_loss_functions import MultiLoss
+
+# todo waiting the implementaion of MultiLoss.warm_up() and MultiLoss.finish_warmup()
