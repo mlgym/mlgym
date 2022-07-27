e7c72fe0ea8df7dabcae64c7011f50c6dc71a419
zengxyu <sduzengxiangyu@outlook.com>
2022-07-27 17:13:11 +0200

    test validation run

--- pytests/gym/test_gym.py
+++ pytests/gym/test_gym.py
@@ -21,22 +21,22 @@ class TestGym(DeviceFixture, ValidationFixtures):
         gym = Gym(process_count, device_ids=device_ids, log_std_to_file=log_std_to_file)
         return gym
 
-    @pytest.fixture
-    def test_blue_print(self, device, grid_search_id, experiment_id, num_epochs, run_mode, experiment_config,
-                        dashify_logging_path, external_injection, job_type):
-        blue_print = LinearBluePrint(grid_search_id=grid_search_id,
-                                     run_id=str(experiment_id),
-                                     epochs=num_epochs,
-                                     run_mode=run_mode,
-                                     config=experiment_config,
-                                     dashify_logging_dir=dashify_logging_path,
-                                     external_injection=external_injection,
-                                     job_type=job_type)
-        gym_job = blue_print.construct(device)
-
-    def test_add_blue_prints(self, blue_print):
-        pass
-
-    def test_run(self):
-        # test blueprints
-        pass
+    # @pytest.fixture
+    # def test_blue_print(self, device, grid_search_id, experiment_id, num_epochs, run_mode, experiment_config,
+    #                     dashify_logging_path, external_injection, job_type):
+    #     blue_print = LinearBluePrint(grid_search_id=grid_search_id,
+    #                                  run_id=str(experiment_id),
+    #                                  epochs=num_epochs,
+    #                                  run_mode=run_mode,
+    #                                  config=experiment_config,
+    #                                  dashify_logging_dir=dashify_logging_path,
+    #                                  external_injection=external_injection,
+    #                                  job_type=job_type)
+    #     gym_job = blue_print.construct(device)
+    #
+    # def test_add_blue_prints(self, blue_print):
+    #     pass
+    #
+    # def test_run(self):
+    #     # test blueprints
+    #     pass
