f6ed2bee3c12f844f76cd9cfe1a5e39a32748d32
zengxyu <sduzengxiangyu@outlook.com>
2022-07-27 16:59:53 +0200

    test validation run

--- pytests/gym/test_gym.py
+++ pytests/gym/test_gym.py
@@ -4,32 +4,37 @@ import pytest
 from ml_gym.gym.gym import Gym
 import torch
 
-from example.conv_net_blueprint import ConvNetBluePrint
 from pytests.test_env.fixtures import DeviceFixture
+from pytests.test_env.linear_net_blueprint import LinearBluePrint
+from pytests.test_env.validation_fixtures import ValidationFixtures
 
 torch.multiprocessing.set_start_method('spawn', force=True)
 
 
-class TestGym(DeviceFixture, ):
+class TestGym(DeviceFixture, ValidationFixtures):
+    @pytest.fixture
+    def experiment_id(self) -> int:
+        return 0
 
     @pytest.fixture
-    def blue_print(self, device):
-        blue_print = ConvNetBluePrint(grid_search_id=grid_search_id,
-                                      run_id=str(experiment_id),
-                                      epochs=num_epochs,
-                                      run_mode=run_mode,
-                                      config=experiment_config,
-                                      dashify_logging_dir=dashify_logging_path,
-                                      external_injection=external_injection,
-                                      job_type=job_type)
-        blue_print.construct(device)
-        pass
-
-    def test_create_gym(self, process_count: int, device_ids, log_std_to_file: bool):
+    def gym(self, process_count: int = 1, device_ids: List[int] = None, log_std_to_file: bool = True):
         gym = Gym(process_count, device_ids=device_ids, log_std_to_file=log_std_to_file)
         return gym
 
-    def test_add_blue_prints(self):
+    @pytest.fixture
+    def test_blue_print(self, device, grid_search_id, experiment_id, num_epochs, run_mode, experiment_config,
+                        dashify_logging_path, external_injection, job_type):
+        blue_print = LinearBluePrint(grid_search_id=grid_search_id,
+                                     run_id=str(experiment_id),
+                                     epochs=num_epochs,
+                                     run_mode=run_mode,
+                                     config=experiment_config,
+                                     dashify_logging_dir=dashify_logging_path,
+                                     external_injection=external_injection,
+                                     job_type=job_type)
+        gym_job = blue_print.construct(device)
+
+    def test_add_blue_prints(self, blue_print):
         pass
 
     def test_run(self):
