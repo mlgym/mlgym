4eecf11fadeb5c1454e1cbcf9431bfc22f2ff7a7
zengxyu <sduzengxiangyu@outlook.com>
2022-07-29 23:39:17 +0200

    test trainer

--- pytests/gym/test_trainer.py
+++ pytests/gym/test_trainer.py
@@ -35,7 +35,7 @@ class TestTrainerComponent(ModelFixture, LossFixture, Postprocessors, DataLoader
         return 1
 
     @pytest.fixture
-    def test_batch(self, data_loader: DatasetLoader, device: torch.device) -> DatasetBatch:
+    def batch(self, data_loader: DatasetLoader, device: torch.device) -> DatasetBatch:
         dataset_batch = list(data_loader)[0]
         dataset_batch.to_device(device=device)
         return dataset_batch
