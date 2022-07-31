70410a1a9f2eaa63a84eb9eefc0db3b52fbf859a
Max Luebbering <le1nux@users.noreply.github.com>
2022-07-31 13:51:34 +0200

    app draft

---
+++ src/ml_gym/frontend/dashboard/src/App.tsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Template from './template/template';
+import FlowBoard from './routes/flow_board'
+import AnalysisBoard from './routes/analysis_board'
+import Settings from './routes/settings'
+import Throughput from './routes/throughput_board'
+import io from 'socket.io-client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useAppDispatch } from "./app/hooks"
+import { pingAdded } from "./features/ping/pingSlice"
+import {IOStatsType} from "./app/datatypes"
+
+
+const socket = io("http://localhost:7000");
+
+
+
+
+export default function App() {
+
+  const [sideBarExpanded, setSideBarExpanded] = useState<boolean>(true);
+  const [selectedPageId, setSelectedPageId] = useState<number>(0)
+  const [ioStats, setIOStats] = useState<IOStatsType>({isConnected: socket.connected, msgTS: [], lastPing: 0, lastPong: 0 });
+  const [msgs, setMsgs] = useState<Array<any>>([]);
+  const appDispatch = useAppDispatch()
+
+
+
+  const setLastPing = (ts: number) => {
+    setIOStats(oldIOStats => ({
+      ...oldIOStats,
+      msgTS: [...oldIOStats["msgTS"]],
+      lastPing: ts
+    }))
+  }
+
+  const setLastPong = (ts: number) => {
+    setIOStats(oldIOStats => ({
+      ...oldIOStats,
+      msgTS: [...oldIOStats["msgTS"]],
+      lastPong: ts
+    }))
+  }
+
+  const setIsConnected = (val: boolean) => {
+    setIOStats(oldIOStats => ({
+      ...oldIOStats,
+      msgTS: [...oldIOStats["msgTS"]],
+      isConnected: val
+    }))
+  }
+
+  const addMsgTs = (ts: number) => {
+    setIOStats(oldIOStats => ({
+      ...oldIOStats,
+      msgTS: [...oldIOStats["msgTS"], ts],
+    }))
+  }
+
+
+  useEffect(() => {
+
+    socket.on('connect', () => {
+      setIsConnected(true);
+      socket.emit('join', { rooms: ['mlgym_event_subscribers'] });
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('mlgym_event', (msg) => {
+      addMsgTs(new Date().getTime())
+      setMsgs(oldArray => [...oldArray, msg])
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().getTime())
+    });
+
+    const interval = setInterval(() => {
+      setLastPing(new Date().getTime())
+      socket.emit('ping');
+    }, 1000);
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('mlgym_event');
+      socket.off('pong');
+      clearInterval(interval);
+    };
+  }, []);
+
+
+
+  const toggleSideBar = () => {
+    setSideBarExpanded(!sideBarExpanded);
+  };
+
+
+
+  const msgs_rep = msgs.map(m => (<div> {JSON.stringify(m)}</div>))
+
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Template toggleSidebar={toggleSideBar} sideBarExpanded={sideBarExpanded} setSelectedPageId={setSelectedPageId} selectedPageId={selectedPageId} >
+          <Routes>
+            <Route
+              path="/flowboard"
+              element={<FlowBoard />}
+            />
+            <Route
+              path="/analysisboard"
+              element={<AnalysisBoard />}
+            />
+            <Route
+              path="/throughput"
+              element={<Throughput ioStats={ioStats} />}
+            />
+            <Route
+              path="/settings"
+              element={<Settings />}
+            />
+
+          </Routes>
+        </Template>
+      </div>
+    </BrowserRouter>);
+}
