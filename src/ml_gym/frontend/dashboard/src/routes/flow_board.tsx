611c2f40571affac669e0d262772d8695d65497b
Max Luebbering <le1nux@users.noreply.github.com>
2022-07-31 16:35:08 +0200

    flow board accesses jobStatus now

--- src/ml_gym/frontend/dashboard/src/routes/flow_board.tsx
+++ src/ml_gym/frontend/dashboard/src/routes/flow_board.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useAppSelector } from "../app/hooks"
+import type { RootState } from '../app/store';
 
 
 type FlowBoardProps = {
 };
 
 
-const FlowBoard: React.FC<FlowBoardProps> = ({}) => {
+const jobIdSelector = (state: RootState) => state.jobStatus.map(s => s.event_id) //s.data.payload.job_id)
+
+const FlowBoard: React.FC<FlowBoardProps> = ({ }) => {
+
+    const jobIDs = useAppSelector(jobIdSelector)
+
 
     return (
-        <h1> Flow Board </h1>
+        <>
+            <h1> Flow Board </h1>
+            <div>{jobIDs}</div>
+        </>
     );
 }
 
