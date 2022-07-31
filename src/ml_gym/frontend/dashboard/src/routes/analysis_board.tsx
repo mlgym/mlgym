4d38dd81bfe4628d6808a7c8de5c9b0b3bf5a338
Max Luebbering <le1nux@users.noreply.github.com>
2022-07-31 13:52:16 +0200

    added analysis board

---
+++ src/ml_gym/frontend/dashboard/src/routes/analysis_board.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+type AnalysisBoardProps = {
+};
+
+
+const AnalysisBoard: React.FC<AnalysisBoardProps> = ({}) => {
+
+    return (
+        <h1> Analysis Board </h1>
+    );
+}
+
+export default AnalysisBoard;
