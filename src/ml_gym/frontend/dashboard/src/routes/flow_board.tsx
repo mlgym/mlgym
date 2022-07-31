373cf273cf6a0eba73d10e19e1e1bd9e18e42eb1
Max Luebbering <le1nux@users.noreply.github.com>
2022-07-31 13:52:55 +0200

    flow board route

---
+++ src/ml_gym/frontend/dashboard/src/routes/flow_board.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+type FlowBoardProps = {
+};
+
+
+const FlowBoard: React.FC<FlowBoardProps> = ({}) => {
+
+    return (
+        <h1> Flow Board </h1>
+    );
+}
+
+export default FlowBoard;
