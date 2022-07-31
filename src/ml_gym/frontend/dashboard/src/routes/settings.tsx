a161afe2b023339118af382b86a4998180b037eb
Max Luebbering <le1nux@users.noreply.github.com>
2022-07-31 13:52:09 +0200

    added settings board

---
+++ src/ml_gym/frontend/dashboard/src/routes/settings.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {useAppSelector} from "../app/hooks"
+
+type SettingsProps = {
+};
+
+
+const Settings: React.FC<SettingsProps> = ({ }) => {
+    return (
+        <>
+        <h1> Settings </h1>
+        </>
+    );
+}
+
+export default Settings;
