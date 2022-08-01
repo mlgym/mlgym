2f9a9ad57b36ea0b7df624b601e462e37e7c1c12
Max Luebbering <le1nux@users.noreply.github.com>
2022-08-01 18:30:04 +0200

    added AG GRID TYPES

--- src/ml_gym/frontend/dashboard/src/app/datatypes.ts
+++ src/ml_gym/frontend/dashboard/src/app/datatypes.ts
@@ -37,7 +37,7 @@ export type JobStatusType = { data: JobStatusInnerType, } & BaseMessageType
 export type ModelStatusPayloadType = {
     experiment_id: number;
     status: string; // <TRAINING, EVALUATING>,,
-    num_epochs: string; //<CALC, TERMINATE>
+    num_epochs: number; //<CALC, TERMINATE>
     current_epoch: number;
     splits: Array<string>;
     current_split: string;
@@ -53,6 +53,32 @@ export type ModelStatusInnerType = {
 export type ModelStatusType = { data: ModelStatusInnerType, } & BaseMessageType
 
 
+// AG GRID TYPES
+
+export type JobStatusRowType = {
+    job_id: number;
+    job_type: string; //<CALC, TERMINATE>
+    job_status: string; //<INIT, RUNNING, DONE>,
+    experiment_id: string; // <path_to_model>
+    starting_time: number;
+    finishing_time: number;
+    error?: string;
+    stacktrace?: string;
+    device?: string // "GPU {1, ..., n}" or "CPU"
+}
+
+export type ModelStatusRowType = {
+    experiment_id: number;
+    model_status: string; // <TRAINING, EVALUATING>,,
+    num_epochs: number; //<CALC, TERMINATE>
+    current_epoch: number;
+    splits: Array<string>;
+    current_split: string;
+    epoch_progress: number;
+    batch_progress: number
+}
+
+
 
 // REDUX STORE
 
@@ -62,8 +88,8 @@ export type JobStatusMessageCollectionType = {
 }
 
 export type ModelStatusMessageCollectionType = {
-    messages: Array<JobStatusType>;
-    experiment_id_to_latest_message_index: { [id: number]: number; }
+    messages: Array<ModelStatusType>;
+    experiment_id_to_latest_message_index: { [id: string]: number; }
 }
 
 
