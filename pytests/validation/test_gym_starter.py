83368ab7a53fab15b4ee54cd3068f8d89cd24455
zengxyu <sduzengxiangyu@outlook.com>
2022-08-12 12:22:37 +0200

    solve             raise SingletonAlreadyInstantiatedError() problem

--- pytests/validation/test_gym_starter.py
+++ pytests/validation/test_gym_starter.py
@@ -59,20 +59,6 @@ class TestGymStarter(LoggingFixture, DeviceFixture, ValidationFixtures):
             assert suffix == num_epochs
         starter._stop_logging_environment()
 
-
-class TestGymStarterCV(LoggingFixture, DeviceFixture, ValidationFixtures):
-    @pytest.fixture
-    def dashify_logging_path(self):
-        return "dashify_logging"
-
-    @pytest.fixture
-    def text_logging_path(self):
-        return "general_logging"
-
-    @pytest.fixture
-    def gpus(self):
-        return [0]
-
     @pytest.mark.parametrize("validation_mode", [(MLGymStarter.ValidationMode.CROSS_VALIDATION)])
     def test_cv_starter(self, blue_print_type,
                         validation_mode,
@@ -107,37 +93,3 @@ class TestGymStarterCV(LoggingFixture, DeviceFixture, ValidationFixtures):
             suffix = int(re.findall(r"\d+", file_name)[0])
             assert suffix == num_epochs
         starter._stop_logging_environment()
-
-    # @pytest.mark.parametrize("validation_mode", [(MLGymStarter.ValidationMode.NESTED_CV)])
-    # def test_nested_cv_starter(self, blue_print_type,
-    #                            validation_mode,
-    #                            num_epochs: int,
-    #                            dashify_logging_path: str,
-    #                            gs_nested_cv_path: str,
-    #                            text_logging_path: str,
-    #                            nested_cv_path: str,
-    #                            process_count: int,
-    #                            gpus: List[int],
-    #                            log_std_to_file: bool,
-    #                            grid_search_id: str,
-    #                            keep_interim_results: bool):
-    #     starter = MLGymStarter(blue_print_class=blue_print_type,
-    #                            validation_mode=validation_mode,
-    #                            dashify_logging_path=dashify_logging_path,
-    #                            text_logging_path=text_logging_path,
-    #                            process_count=process_count,
-    #                            gpus=gpus,
-    #                            log_std_to_file=log_std_to_file,
-    #                            gs_config_path=gs_nested_cv_path,
-    #                            evaluation_config_path=nested_cv_path,
-    #                            num_epochs=num_epochs,
-    #                            keep_interim_results=keep_interim_results)
-    #     starter.start()
-    #
-    #     model_paths = glob.glob(os.path.join(dashify_logging_path, grid_search_id, "**/**/**/model_*.pt"))
-    #     assert len(model_paths) != 0
-    #
-    #     for model_path in model_paths:
-    #         file_name = os.path.basename(model_path)
-    #         suffix = int(re.findall(r"\d+", file_name)[0])
-    #         assert suffix == num_epochs
