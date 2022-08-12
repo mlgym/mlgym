7ebd8476a0e561ceb8ca5e98c4d6f60ab272cef0
zengxyu <sduzengxiangyu@outlook.com>
2022-08-12 12:59:44 +0200

    code  quality

--- pytests/test_env/validation_fixtures.py
+++ pytests/test_env/validation_fixtures.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import os
 from typing import Type, Dict, Any
 
 import pytest
@@ -7,45 +6,13 @@ from ml_gym.io.config_parser import YAMLConfigLoader
 from pytests.test_env.linear_net_blueprint import LinearBluePrint
 
 
-@pytest.fixture
-def gs_cv_config_path() -> str:
-    # gs_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env", "cross_validation/gs_config_cv.yml")
-    gs_cv_path = "pytests/test_env/cross_validation/gs_config_cv.yml"
-    return gs_cv_path
-
-
-@pytest.fixture
-def cv_config_path() -> str:
-    # cv_path = os.path.join(os.path.abspath('.'), "..", "test_env", "cross_validation/cv_config.yml")
-    cv_path = "pytests/test_env/cross_validation/cv_config.yml"
-    return cv_path
-
-
-@pytest.fixture
-def gs_config_path() -> str:
-    # gs_path = os.path.join(os.path.abspath('.'), "..", "test_env", "grid_search/gs_config.yml")
-    gs_path = "pytests/test_env/grid_search/gs_config.yml"
-    return gs_path
-
-
-@pytest.fixture
-def gs_nested_cv_path() -> str:
-    # gs_nested_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env",
-    #                                  "nested_cross_validation/gs_config_nested_cv.yml")
-    gs_nested_cv_path = "pytests/test_env/nested_cross_validation/gs_config_nested_cv.yml"
-    return gs_nested_cv_path
-
-
-@pytest.fixture
-def nested_cv_path() -> str:
-    # nested_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env",
-    #                               "nested_cross_validation/nested_cv_config.yml")
-    nested_cv_path = "pytests/test_env/nested_cross_validation/nested_cv_config.yml"
-
-    return nested_cv_path
-
-
 class ValidationFixtures:
+
+    @pytest.fixture
+    def gs_cv_config_path(cls) -> str:
+        # gs_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env", "cross_validation/gs_config_cv.yml")
+        gs_cv_path = "pytests/test_env/cross_validation/gs_config_cv.yml"
+        return gs_cv_path
 
     @pytest.fixture
     def gs_cv_config(self, gs_cv_config_path) -> Dict[str, Any]:
@@ -53,9 +20,21 @@ class ValidationFixtures:
         return gs_cv_config
 
     @pytest.fixture
+    def cv_config_path(self) -> str:
+        # cv_path = os.path.join(os.path.abspath('.'), "..", "test_env", "cross_validation/cv_config.yml")
+        cv_path = "pytests/test_env/cross_validation/cv_config.yml"
+        return cv_path
+
+    @pytest.fixture
     def cv_config(self, cv_config_path) -> Dict[str, Any]:
         cv_config = YAMLConfigLoader.load(cv_config_path)
         return cv_config
+
+    @pytest.fixture
+    def gs_config_path(self) -> str:
+        # gs_path = os.path.join(os.path.abspath('.'), "..", "test_env", "grid_search/gs_config.yml")
+        gs_path = "pytests/test_env/grid_search/gs_config.yml"
+        return gs_path
 
     @pytest.fixture
     def gs_config(self, gs_config_path) -> Dict[str, Any]:
@@ -63,9 +42,24 @@ class ValidationFixtures:
         return gs_config
 
     @pytest.fixture
+    def gs_nested_cv_path(self) -> str:
+        # gs_nested_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env",
+        #                                  "nested_cross_validation/gs_config_nested_cv.yml")
+        gs_nested_cv_path = "pytests/test_env/nested_cross_validation/gs_config_nested_cv.yml"
+        return gs_nested_cv_path
+
+    @pytest.fixture
     def gs_nested_cv_config(self, gs_nested_cv_path) -> Dict[str, Any]:
         gs_nested_cv_config = YAMLConfigLoader.load(gs_nested_cv_path)
         return gs_nested_cv_config
+
+    @pytest.fixture
+    def nested_cv_path(self) -> str:
+        # nested_cv_path = os.path.join(os.path.abspath('.'), "..", "test_env",
+        #                               "nested_cross_validation/nested_cv_config.yml")
+        nested_cv_path = "pytests/test_env/nested_cross_validation/nested_cv_config.yml"
+
+        return nested_cv_path
 
     @pytest.fixture
     def nested_cv_config(self, nested_cv_path) -> Dict[str, Any]:
