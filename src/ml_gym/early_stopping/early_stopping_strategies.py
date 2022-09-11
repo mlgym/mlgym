fc0b39271049cc346411dd97e69aae0d1b7bd322
Max Luebbering <le1nux@users.noreply.github.com>
2022-09-11 11:27:24 +0200

    early stopping criterion now working

--- src/ml_gym/early_stopping/early_stopping_strategies.py
+++ src/ml_gym/early_stopping/early_stopping_strategies.py
@@ -1,6 +1,7 @@
 from typing import List
 from ml_gym.batching.batch import EvaluationBatchResult
 from ml_gym.error_handling.exception import BatchStateError
+import numpy as np
 
 
 class EarlyStoppingIF:
@@ -19,14 +20,20 @@ class EarlyStopping(EarlyStoppingIF):
 
 class LastKEpochsImprovementStrategy(EarlyStoppingIF):
 
-    def __init__(self, init_value: float, min_percentage_improvement: float, epochs_window: int, split_name: str, monitoring_key: str,
+    def __init__(self, min_relative_improvement: float, epochs_window: int, split_name: str, monitoring_key: str,
                  is_increase_task: bool):
-        self.monitoring_values = [init_value]*epochs_window
-        self.min_percentage_improvement = min_percentage_improvement
+        monitoring_values_list = [1]
+        init_min_relative_improvement = min_relative_improvement * 1.1  # we need to make this a bit bigger for precision inaccuracies
+        for i in range(1, epochs_window):
+            monitoring_values_list.append(monitoring_values_list[i-1] + monitoring_values_list[i-1]*init_min_relative_improvement)
+        self.monitoring_values = np.array(monitoring_values_list)
+        if not is_increase_task:
+            self.monitoring_values = self.monitoring_values[::-1]
+        self.min_relative_improvement = min_relative_improvement
         self.epochs_window = epochs_window
         self.split_name = split_name
         self.monitoring_key = monitoring_key
-        self.diff_filter_fun = min if is_increase_task else max
+        self.is_increase_task = is_increase_task
 
     def _get_monitoring_value(self, evaluation_results: List[EvaluationBatchResult]) -> float:
         evaluation_result = None
@@ -42,13 +49,20 @@ class LastKEpochsImprovementStrategy(EarlyStoppingIF):
 
     def _evaluate_history(self, current_epoch: int) -> bool:
         def monitoring_diff_fun(i, current_epoch):
-            return self.monitoring_values[(i + current_epoch) % self.epochs_window] - self.monitoring_values[(i-1 + current_epoch) % self.epochs_window]
-        diffs = [monitoring_diff_fun(i, current_epoch) for i in range(self.epochs_window-1)]
-        return abs(self.diff_filter_fun(diffs)) < self.min_percentage_improvement
+            next_val = self.monitoring_values[(i + 2 + current_epoch) % self.epochs_window]
+            base_val = self.monitoring_values[(i + 1 + current_epoch) % self.epochs_window]
+            relative_diff = (next_val - base_val) / base_val
+            return relative_diff
+
+        relative_diffs = [monitoring_diff_fun(i, current_epoch) for i in range(self.epochs_window-1)]
+        if self.is_increase_task:
+            return max(relative_diffs) < self.min_relative_improvement
+        else:
+            return min(relative_diffs) > self.min_relative_improvement * -1
 
     def is_stopping_criterion_fulfilled(self, evaluation_results: List[EvaluationBatchResult], current_epoch: int) -> bool:
         monitoring_value = self._get_monitoring_value(evaluation_results)
-        self.monitoring_values[current_epoch-1 % self.epochs_window] = monitoring_value
+        self.monitoring_values[current_epoch % self.epochs_window] = monitoring_value
 
         perform_stop = self._evaluate_history(current_epoch)
         return perform_stop
@@ -56,10 +70,9 @@ class LastKEpochsImprovementStrategy(EarlyStoppingIF):
 
 class EarlyStoppingStrategyFactory:
 
-    def get_last_k_epochs_improvement_strategy(init_value: float, min_percentage_improvement: float, epochs_window: int, split_name: str,
+    def get_last_k_epochs_improvement_strategy(min_relative_improvement: float, epochs_window: int, split_name: str,
                                                monitoring_key: str, is_increase_task: bool) -> EarlyStoppingIF:
-        strategy = LastKEpochsImprovementStrategy(init_value=init_value,
-                                                  min_percentage_improvement=min_percentage_improvement,
+        strategy = LastKEpochsImprovementStrategy(min_relative_improvement=min_relative_improvement,
                                                   epochs_window=epochs_window,
                                                   split_name=split_name,
                                                   monitoring_key=monitoring_key,
