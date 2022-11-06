283069d65980101dce8162b08503ca9c8f7aa55b
Max Luebbering <le1nux@users.noreply.github.com>
2022-11-06 16:06:24 +0100

    added alert message component to mlboard

--- src/ml_board/frontend/dashboard/src/index.tsx
+++ src/ml_board/frontend/dashboard/src/index.tsx
@@ -4,17 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import {store} from './app/store'
+import { store } from './app/store'
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
       <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>
+        <App />
+      </Provider>
+    </React.StrictMode>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
