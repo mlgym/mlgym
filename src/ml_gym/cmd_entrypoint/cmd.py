9a72518483da7f9c503f2c06d25b6e62939c5f6d
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2023-05-03 15:27:37 +0200

    remove refactor

--- src/ml_gym/cmd_entrypoint/cmd.py
+++ src/ml_gym/cmd_entrypoint/cmd.py
@@ -5,22 +5,15 @@ from ml_board.backend.restful_api.data_models import FileFormat
 from ml_gym.blueprints.blue_prints import BluePrint
 from ml_gym.error_handling.exception import GymError
 from ml_gym.gym.gym import Gym, GymFactory
-from ml_gym.persistency.logging import (
-    MLgymStatusLoggerCollectionConstructable,
-    LoggerConstructableIF,
-    MLgymStatusLoggerConfig,
-    MLgymStatusLoggerTypes,
-)
+from ml_gym.persistency.logging import MLgymStatusLoggerCollectionConstructable, LoggerConstructableIF, MLgymStatusLoggerConfig, \
+    MLgymStatusLoggerTypes
 from ml_gym.modes import RunMode, ValidationMode
+from ml_gym.util.util import SystemEnv
 from ml_gym.validation.validator_factory import get_validator
 from ml_gym.io.config_parser import YAMLConfigLoader
 from typing import Callable, List, Tuple, Type, Dict, Union
-from ml_gym.persistency.io import (
-    GridSearchAPIClientConfig,
-    GridSearchAPIClientConstructableIF,
-    GridSearchAPIClientConstructable,
-    GridSearchAPIClientType,
-)
+from ml_gym.persistency.io import GridSearchAPIClientConfig, GridSearchAPIClientConstructableIF, GridSearchAPIClientConstructable, \
+    GridSearchAPIClientType
 from pathlib import Path
 import yaml
 from accelerate import Accelerator
@@ -83,19 +76,19 @@ class LoggingConfiguration:
     :params:
         websocket_logging_servers(List[str]): Endpoint for Websocket Server.\n
         gs_rest_api_endpoint (str): Endpoint for RESTApi .
+
     """
     websocket_logging_servers: List[str]
     gs_rest_api_endpoint: str
 
 
-def get_gym_from_environment_config(
-    env_config: Union[MultiProcessingEnvironmentConfig, MainProcessEnvironmentConfig, AccelerateEnvironmentConfig],
-    logger_collection_constructable: LoggerConstructableIF,
-    gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
-    num_epochs: int,
-    num_batches_per_epoch: int = None,
-    accelerator: Accelerator = None,
-) -> Gym:
+def get_gym_from_environment_config(env_config: Union[MultiProcessingEnvironmentConfig, MainProcessEnvironmentConfig,
+                                                      AccelerateEnvironmentConfig],
+                                    logger_collection_constructable: LoggerConstructableIF,
+                                    gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
+                                    num_epochs: int,
+                                    num_batches_per_epoch: int = None,
+                                    accelerator: Accelerator = None) -> Gym:
     """
     Create Gym class object from the run configuration
 
@@ -112,29 +105,23 @@ def get_gym_from_environment_config(
     """
 
     if isinstance(env_config, MainProcessEnvironmentConfig):
-        gym = GymFactory.get_sequential_gym(
-            logger_collection_constructable=logger_collection_constructable,
-            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-            device_id=env_config.computation_device_id,
-            num_batches_per_epoch=num_batches_per_epoch,
-        )
+        gym = GymFactory.get_sequential_gym(logger_collection_constructable=logger_collection_constructable,
+                                            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
+                                            device_id=env_config.computation_device_id,
+                                            num_batches_per_epoch=num_batches_per_epoch)
     elif isinstance(env_config, AccelerateEnvironmentConfig):
-        gym = GymFactory.get_sequential_gym(
-            logger_collection_constructable=logger_collection_constructable,
-            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-            num_epochs=num_epochs,
-            num_batches_per_epoch=num_batches_per_epoch,
-            accelerator=accelerator,
-        )
+        gym = GymFactory.get_sequential_gym(logger_collection_constructable=logger_collection_constructable,
+                                            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
+                                            num_epochs=num_epochs,
+                                            num_batches_per_epoch=num_batches_per_epoch,
+                                            accelerator=accelerator)
     elif isinstance(env_config, MultiProcessingEnvironmentConfig):
-        gym = GymFactory.get_parallel_single_node_gym(
-            logger_collection_constructable=logger_collection_constructable,
-            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-            process_count=env_config.process_count,
-            num_epochs=num_epochs,
-            device_ids=env_config.computation_device_ids,
-            num_batches_per_epoch=num_batches_per_epoch,
-        )
+        gym = GymFactory.get_parallel_single_node_gym(logger_collection_constructable=logger_collection_constructable,
+                                                      gs_restful_api_client_constructable=gs_restful_api_client_constructable,
+                                                      process_count=env_config.process_count,
+                                                      num_epochs=num_epochs,
+                                                      device_ids=env_config.computation_device_ids,
+                                                      num_batches_per_epoch=num_batches_per_epoch)
     else:
         raise GymError("Did not provide correct env_config")
 
@@ -150,14 +137,11 @@ def get_logger_constructable(websocket_logging_servers: List) -> LoggerConstruct
 
     :returns:  logger_collection_constructable (LoggerConstructableIF): Initiated Logging interface
     """
-    websocket_logging_servers = [
-        (f"{protocol}:{ip}", int(port)) for protocol, ip, port in [connection.split(":") for connection in websocket_logging_servers]
-    ]
+    websocket_logging_servers = [(f"{protocol}:{ip}", int(port)) for protocol, ip, port in [
+        connection.split(":") for connection in websocket_logging_servers]]
 
-    logging_configs = [
-        MLgymStatusLoggerConfig(logger_type=MLgymStatusLoggerTypes.STREAMED_LOGGER, logger_config={"host": ip, "port": port})
-        for ip, port in websocket_logging_servers
-    ]
+    logging_configs = [MLgymStatusLoggerConfig(logger_type=MLgymStatusLoggerTypes.STREAMED_LOGGER, logger_config={"host": ip, "port": port})
+                       for ip, port in websocket_logging_servers]
     logger_collection_constructable = MLgymStatusLoggerCollectionConstructable(logging_configs)
     return logger_collection_constructable
 
@@ -171,22 +155,15 @@ def get_grid_search_restful_api_client_constructable(endpoint: str) -> GridSearc
 
     :returns:  client_constructable (GridSearchAPIClientConstructableIF): Initiated Grid Search API Client interface for performing REST calls.
     """
-    client_config = GridSearchAPIClientConfig(
-        api_client_type=GridSearchAPIClientType.GRID_SEARCH_RESTFUL_API_CLIENT, api_client_config={"endpoint": endpoint}
-    )
+    client_config = GridSearchAPIClientConfig(api_client_type=GridSearchAPIClientType.GRID_SEARCH_RESTFUL_API_CLIENT,
+                                              api_client_config={"endpoint": endpoint})
     client_constructable = GridSearchAPIClientConstructable(client_config)
     return client_constructable
 
 
-def entry_train(
-    gridsearch_id: str,
-    blueprint_class: Type[BluePrint],
-    gym: Gym,
-    gs_config_path: str,
-    validation_strategy_config_path: str,
-    gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
-    accelerator: Accelerator = None,
-):
+def entry_train(gridsearch_id: str, blueprint_class: Type[BluePrint], gym: Gym, gs_config_path: str, run_config_path: str,
+                validation_strategy_config_path: str, gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
+                accelerator: Accelerator = None):
     
     """
     Create Gym class object from the run configuration
@@ -202,12 +179,9 @@ def entry_train(
         accelerator (Accelerator): Accelerator object used for distributed training over multiple GPUs
     """
 
-    def log_configs(
-        gs_config_string: str,
-        validation_strategy_config_raw_string: str,
-        gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
-        accelerator: Accelerator = None,
-    ):
+
+    def log_configs(gs_config_string: str, run_config_string: str, validation_strategy_config_raw_string: str,
+                    gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF, accelerator: Accelerator = None):
         """
         Log the configuration files in event_storage which are being used to run Gym Model.
 
@@ -220,26 +194,31 @@ def entry_train(
     """
         if accelerator is None or accelerator.is_main_process:
             gs_api_client = gs_restful_api_client_constructable.construct()
-            gs_api_client.add_config_string(
-                grid_search_id=gridsearch_id, file_format=FileFormat.YAML, config_name="grid_search_config.yml", config=gs_config_string
-            )
+            gs_api_client.add_config_string(grid_search_id=gridsearch_id,
+                                            file_format=FileFormat.YAML,
+                                            config_name="grid_search_config.yml",
+                                            config=gs_config_string)
+            gs_api_client.add_config_string(grid_search_id=gridsearch_id, 
+                                            file_format=FileFormat.YAML, 
+                                            config_name="run_config.yml", 
+                                            config=run_config_string)
 
             if validation_strategy_config_raw_string is not None:
-                gs_api_client.add_config_string(
-                    grid_search_id=gridsearch_id, config_name="validation_strategy_config.yml", config=validation_strategy_config_raw_string
-                )
+                gs_api_client.add_config_string(grid_search_id=gridsearch_id,
+                                                config_name="validation_strategy_config.yml",
+                                                config=validation_strategy_config_raw_string)
 
             for blueprint in blueprints:
-                gs_api_client.add_config_string(
-                    grid_search_id=blueprint.grid_search_id,
-                    config_name="experiment_config.json",
-                    config=json.dumps(blueprint.config),
-                    experiment_id=blueprint.experiment_id,
-                    file_format=FileFormat.JSON,
-                )
+                gs_api_client.add_config_string(grid_search_id=blueprint.grid_search_id, config_name="experiment_config.json",
+                                                config=json.dumps(blueprint.config), experiment_id=blueprint.experiment_id,
+                                                file_format=FileFormat.JSON)
+                gs_api_client.add_config_string(grid_search_id=blueprint.grid_search_id, config_name="system_info.json",
+                                                config=json.dumps(SystemEnv.create_system_info()), experiment_id=blueprint.experiment_id,
+                                                file_format=FileFormat.JSON)
 
     gs_config_string = Path(gs_config_path).read_text()
     gs_config = YAMLConfigLoader.load_string(gs_config_string)
+    run_config_string = Path(run_config_path).read_text()
 
     if validation_strategy_config_path is not None:
         validation_strategy_config_string = Path(validation_strategy_config_path).read_text()
@@ -254,25 +233,18 @@ def entry_train(
 
     validator = get_validator(validation_mode, blueprint_class, RunMode.TRAIN, validation_strategy_config, gs_config)
 
-    blueprints = validator.create_blueprints(grid_search_id=gridsearch_id, blue_print_type=blueprint_class, gs_config=gs_config)
+    blueprints = validator.create_blueprints(grid_search_id=gridsearch_id,
+                                             blue_print_type=blueprint_class,
+                                             gs_config=gs_config)
 
-    log_configs(
-        gs_config_string=gs_config_string,
-        validation_strategy_config_raw_string=validation_strategy_config_string,
-        gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-        accelerator=accelerator,
-    )
+    log_configs(gs_config_string=gs_config_string, run_config_string=run_config_string,      validation_strategy_config_raw_string=validation_strategy_config_string,
+    gs_restful_api_client_constructable=gs_restful_api_client_constructable, accelerator=accelerator)
 
     gym.run(blueprints)
 
 
-def entry_warm_start(
-    blueprint_class: Type[BluePrint],
-    gym: Gym,
-    grid_search_id: int,
-    gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF,
-    num_epochs: int,
-):
+def entry_warm_start(blueprint_class: Type[BluePrint], gym: Gym, grid_search_id: int,
+                     gs_restful_api_client_constructable: GridSearchAPIClientConstructableIF, num_epochs: int):
     """
     Create Gym class object from the run configuration
 
@@ -288,18 +260,13 @@ def entry_warm_start(
     gs_api_client = gs_restful_api_client_constructable.construct()
     experiment_statuses = gs_api_client.get_experiment_statuses(grid_search_id)
 
-    blueprints = [
-        BluePrint.create_blueprint(
-            blue_print_class=blueprint_class,
-            run_mode=RunMode.WARM_START,
-            experiment_config=experiment_status.experiment_config,
-            warm_start_epoch=experiment_status.last_checkpoint_id,
-            grid_search_id=grid_search_id,
-            experiment_id=experiment_status.experiment_id,
-        )
-        for experiment_status in experiment_statuses
-        if experiment_status.last_checkpoint_id < num_epochs
-    ]
+    blueprints = [BluePrint.create_blueprint(blue_print_class=blueprint_class,
+                                             run_mode=RunMode.WARM_START,
+                                             experiment_config=experiment_status.experiment_config,
+                                             warm_start_epoch=experiment_status.last_checkpoint_id,
+                                             grid_search_id=grid_search_id,
+                                             experiment_id=experiment_status.experiment_id)
+                  for experiment_status in experiment_statuses if experiment_status.last_checkpoint_id < num_epochs]
 
     gym.run(blueprints)
 
@@ -310,21 +277,17 @@ def get_args() -> Tuple[Callable, Dict]:
 
     :returns:  args (Tuple[Callable, Dict]): run_config passes with --config_path.
     """
-    parser = argparse.ArgumentParser(description="Run a grid search on CPUs or distributed over multiple GPUs")
+    parser = argparse.ArgumentParser(description='Run a grid search on CPUs or distributed over multiple GPUs')
 
-    parser.add_argument("--config_path", type=str, required=True, help="Path to the run configuration file")
+    parser.add_argument('--config_path', type=str, required=True, help='Path to the run configuration file')
 
     args = parser.parse_args()
     return args.config_path
 
 
-def parse_run_configuration(
-    run_configuration_file_path: str,
-) -> Tuple[
-    Union[TrainRunConfiguration, WarmStartRunConfiguration],
-    Union[MultiProcessingEnvironmentConfig, AccelerateEnvironmentConfig, MainProcessEnvironmentConfig],
-    LoggingConfiguration,
-]:
+def parse_run_configuration(run_configuration_file_path: str) -> Tuple[Union[TrainRunConfiguration, WarmStartRunConfiguration],
+                                                                       Union[MultiProcessingEnvironmentConfig, AccelerateEnvironmentConfig,
+                                                                       MainProcessEnvironmentConfig], LoggingConfiguration]:
     """
     Parsing run_config.yml file to create configs needed for mlGym to run experiments.
 
@@ -392,34 +355,19 @@ def run(blueprint_class: BluePrint, run_configuration_file_path):
     run_config, env_config, logging_config = parse_run_configuration(run_configuration_file_path=run_configuration_file_path)
 
     logger_collection_constructable, gs_restful_api_client_constructable = get_logging_constructables(logging_config)
-
+    
     accelerator: Accelerator = Accelerator() if isinstance(env_config, AccelerateEnvironmentConfig) else None
 
-    gym = get_gym_from_environment_config(
-        env_config,
-        logger_collection_constructable,
-        gs_restful_api_client_constructable,
-        run_config.num_epochs,
-        run_config.num_batches_per_epoch,
-        accelerator,
-    )
+    gym = get_gym_from_environment_config(env_config, logger_collection_constructable, gs_restful_api_client_constructable,
+                                          run_config.num_epochs, run_config.num_batches_per_epoch, accelerator)
+
 
     if isinstance(run_config, TrainRunConfiguration):
         gridsearch_id = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
-        entry_train(
-            gridsearch_id=gridsearch_id,
-            blueprint_class=blueprint_class,
-            gym=gym,
-            gs_config_path=run_config.gs_config_path,
-            validation_strategy_config_path=run_config.validation_config_path,
-            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-            accelerator=accelerator,
-        )
+        entry_train(gridsearch_id=gridsearch_id, blueprint_class=blueprint_class, gym=gym, gs_config_path=run_config.gs_config_path,
+                    run_config_path=run_configuration_file_path, validation_strategy_config_path=run_config.validation_config_path,
+                    gs_restful_api_client_constructable=gs_restful_api_client_constructable,
+                    accelerator=accelerator)
     elif isinstance(run_config, WarmStartRunConfiguration):
-        entry_warm_start(
-            blueprint_class=blueprint_class,
-            gym=gym,
-            grid_search_id=run_config.gridsaerch_id,
-            gs_restful_api_client_constructable=gs_restful_api_client_constructable,
-            num_epochs=run_config.num_epochs,
-        )
+        entry_warm_start(blueprint_class=blueprint_class, gym=gym, grid_search_id=run_config.gridsaerch_id,
+                         gs_restful_api_client_constructable=gs_restful_api_client_constructable, num_epochs=run_config.num_epochs)
