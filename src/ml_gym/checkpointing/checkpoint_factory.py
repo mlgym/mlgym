a5a70c01361b9eb7330b80cb195fff21976990b4
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2023-05-24 17:21:23 +0200

    Revert "Revert "Merge branch 'mlgym#174-docum' into dev""
    This reverts commit fec20ce56783ae4c61d203c492a1a4dc7c3e8f2d.

--- src/ml_gym/checkpointing/checkpoint_factory.py
+++ src/ml_gym/checkpointing/checkpoint_factory.py
@@ -3,6 +3,10 @@ from ml_gym.checkpointing.checkpointing import SaveLastEpochOnlyCheckpointingStr
 
 
 class CheckpointingStrategyFactory:
+    """
+    Checkpointing Stratergy Class used for finding what stratergy to use for 
+    creating checkpoints for the experiments while running MlGym Job.
+    """
     @staticmethod
     def get_save_last_epoch_only_checkpointing_strategy() -> CheckpointingIF:
         strategy = SaveLastEpochOnlyCheckpointingStrategy()
