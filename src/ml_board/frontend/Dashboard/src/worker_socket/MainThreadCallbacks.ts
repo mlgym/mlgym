e9ac896052815e7970b20ab23b81136fc2706a25
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2023-05-25 14:38:45 +0200

    210 fix single msgs after batching (#230)
    * restored the deleted line for emitting single messages
    
    * removing old code
    
    * rewriting some the buffering in MainThreadCallbacks

--- src/ml_board/frontend/Dashboard/src/worker_socket/MainThreadCallbacks.ts
+++ src/ml_board/frontend/Dashboard/src/worker_socket/MainThreadCallbacks.ts
@@ -17,9 +17,30 @@ const MapEventToProcess: { [event: string]: (output: DataToRedux, input: JSON) =
     },
     [MLGYM_EVENT.EXPERIMENT_CONFIG]: (dataToRedux: DataToRedux, data: JSON): void => console.log("Exp config found"),
     [MLGYM_EVENT.EXPERIMENT_STATUS]: (dataToRedux: DataToRedux, data: JSON): void => { dataToRedux.tableData.push(handleExperimentStatusData(data)) },
-    // [MLGYM_EVENT.BATCHED_EVENTS]: (dataToRedux: DataToRedux, data: JSON): void => { dataToRedux.tableData.push(handleJobStatusData(data)) },
 };
 
+// ========================= helper methods ============================//
+
+// loops over buffers, whether created by the websocket or received as a buffered message
+function processBuffer(bufferedSocketData: Array<JSON>, bufferedDataToRedux: DataToRedux) {
+    // loop over all incoming data from socket
+    for (const data of bufferedSocketData) {
+        // assume incoming data type to be BufferedDataFromSocket and verify the event_id
+        const data_from_socket = data as BufferedDataFromSocket;
+        if (data_from_socket.event_id === "batched_events") {
+            // if that's the case then do a recursive call on the data and skip afterwards
+            processBuffer(data_from_socket.data, bufferedDataToRedux)
+            continue;
+            // NOTE: due to the nature of the incoming messages we know that there is 
+            // never a BufferedDataFromSocket inside of another BufferedDataFromSocket
+            // so it's safe to say that this recursion is 1-level-deep only!
+        }
+        // parse data from socket then extract event_type and payload
+        const { data: { event_type, payload } } = data as DataFromSocket;
+        // process the payload and load it into the DataToRedux object
+        MapEventToProcess[event_type as keyof typeof MapEventToProcess](bufferedDataToRedux, payload);
+    }
+}
 
 // ========================= Callbacks to update the MainThread ============================//
 export const updateMainThreadCallback = (bufferedSocketData: Array<JSON>) => {
@@ -28,27 +49,8 @@ export const updateMainThreadCallback = (bufferedSocketData: Array<JSON>) => {
         tableData: [],
         chartsUpdates: []
     };
-    // loop over all incoming data from socket
-    for (const bfrdata of bufferedSocketData) {
-        let data_from_socket = bfrdata as DataFromSocket | BufferedDataFromSocket;
-        // handling batched events coming from socket itself
-        if(data_from_socket.event_id === "batched_events") {
-            const buffered_data =  data_from_socket as BufferedDataFromSocket;
-            // treating batched events as normal buffered events from web workers (like the else part of this function)
-            for(const iterated_buffer_data of buffered_data["data"]) {
-                // parse data from socket then extract event_type and payload
-                const { data: { event_type, payload } } = iterated_buffer_data as DataFromSocket;
-                // process the payload and load it into the DataToRedux object
-                MapEventToProcess[event_type as keyof typeof MapEventToProcess](bufferedDataToRedux, payload);
-            }
-        }
-        else {
-            // parse data from socket then extract event_type and payload
-            const { data: { event_type, payload } } = data_from_socket as DataFromSocket;
-            // process the payload and load it into the DataToRedux object
-            MapEventToProcess[event_type as keyof typeof MapEventToProcess](bufferedDataToRedux, payload);
-        }
-    }
+    // Process the buffer coming from the socket and populate DataToRedux object accordingly
+    processBuffer(bufferedSocketData, bufferedDataToRedux);
     // sending Data to the Main thread to store it in Redux
     postMessage(bufferedDataToRedux);
 };
