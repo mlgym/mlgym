d23e172e933c25459d1b9f36580d9925fea928db
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2023-11-19 20:09:02 +0100

    experiment shell script

---
+++ example/gpt2/huggingface_impl/run_experiments.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CUDA_VISIBLE_DEVICES='1' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
+
+CUDA_VISIBLE_DEVICES='1,2' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
+
+CUDA_VISIBLE_DEVICES='1,2,3' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
+
+CUDA_VISIBLE_DEVICES='1,2,3,4' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
+
+CUDA_VISIBLE_DEVICES='1,2,3,4,5' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py
+
+CUDA_VISIBLE_DEVICES='1,2,3,4,5,6' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
+
+CUDA_VISIBLE_DEVICES='1,2,3,4,5,6,7' python3 /cluster/home/mlgym/example/gpt2/huggingface_impl/gpt2_hug_impl.py 
